ede6da2d4aa1c4d767f1d08c4e2dc072de20f57f
kolen123321 <57564910+kolen123321@users.noreply.github.com>
2022-05-08 12:30:20 +0300

    Delete verify directory

--- verify/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import VerifyCode
-
-class VerifyCodeSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = VerifyCode
-        fields = '__all__'
